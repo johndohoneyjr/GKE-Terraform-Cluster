0ccf618c2e241656cc049f1c8be965d2422bbc24
John Dohoney <42818490+johndohoneyjr@users.noreply.github.com>
2020-03-20 16:31:46 -0700

    Updates to 0.12

--- main.tf
+++ main.tf
@@ -1,11 +1,8 @@
 terraform {
-  required_version = ">= 0.11.0"
+  required_version = ">= 0.12.0"
 }
 
 provider "google" {
- credentials = "${var.gcp_credentials}"
- project     = "${var.gcp_project}"
- region      = "${var.gcp_region}"
 }
 
 resource "google_compute_network" "my-net" {
@@ -14,7 +11,7 @@ resource "google_compute_network" "my-net" {
 
 resource "google_compute_firewall" "my-firewall" {
   name    = "ops-manager-fw"
-  network = "${google_compute_network.my-net.name}"
+  network = google_compute_network.my-net.name
 
   allow {
     protocol = "icmp"
@@ -29,31 +26,29 @@ resource "google_compute_firewall" "my-firewall" {
 }
 
 data "google_container_engine_versions" "west" {
-  location = "${var.gcp_zone}"
+  location = var.gcp_zone
 }
 
-resource "google_container_cluster" "k8sexample" {
-  name               = "${var.cluster_name}"
-  description        = "example k8s cluster"
-  location               = "${var.gcp_zone}"
-  initial_node_count = "${var.initial_node_count}"
-  enable_kubernetes_alpha = "true"
-  enable_legacy_abac = "true"
+resource "google_container_cluster" "opsmanager" {
+  name                    = var.cluster_name
+  description             = "example k8s cluster"
+  location                = var.gcp_zone
+  initial_node_count      = var.initial_node_count
+  enable_legacy_abac      = "true"
 
   master_auth {
-    username = "${var.master_username}"
-    password = "${var.master_password}"
+    username = var.master_username
+    password = var.master_password
   }
 
   node_config {
-    machine_type = "${var.node_machine_type}"
-    disk_size_gb = "${var.node_disk_size}"
+    machine_type = var.node_machine_type
+    disk_size_gb = var.node_disk_size
     oauth_scopes = [
       "https://www.googleapis.com/auth/compute",
       "https://www.googleapis.com/auth/devstorage.read_only",
       "https://www.googleapis.com/auth/logging.write",
-      "https://www.googleapis.com/auth/monitoring"
+      "https://www.googleapis.com/auth/monitoring",
     ]
   }
 }
-
