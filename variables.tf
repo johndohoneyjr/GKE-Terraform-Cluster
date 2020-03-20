99e6edf336fc3e7bded3e82a28bc0c4ddd86ea3c
John Dohoney <42818490+johndohoneyjr@users.noreply.github.com>
2020-03-20 16:33:29 -0700

    0.12 updates

--- variables.tf
+++ variables.tf
@@ -13,50 +13,37 @@ variable "gcp_project" {
   default     = "dohoneydemos"
 }
 
-variable "gcp_credentials" {
-  description = "GCP project credentials"
-  default     = "/Users/johndohoney/.config/gcloud/dohoneydemos-40ad4f4d925a.json"
-}
-
 variable "cluster_name" {
   description = "Name of the K8s cluster"
-  default = "k8s-cluster"
+  default     = "k8s-cluster"
 }
 
 variable "initial_node_count" {
   description = "Number of worker VMs to initially create"
-  default = 3
+  default     = 3
 }
 
 variable "master_username" {
   description = "Username for accessing the Kubernetes master endpoint"
-  default = "k8smaster"
+  default     = "k8smaster"
 }
 
 variable "master_password" {
   description = "Password for accessing the Kubernetes master endpoint"
-  default = "k8smasterk8smaster"
+  default     = "k8smasterk8smaster"
 }
 
 variable "node_machine_type" {
   description = "GCE machine type"
-  default = "n1-standard-2"
+  default     = "n1-standard-2"
 }
 
 variable "node_disk_size" {
   description = "Node disk size in GB"
-  default = "20"
+  default     = "20"
 }
 
 variable "environment" {
   description = "value passed to ACS Environment tag"
-  default = "dev"
+  default     = "dev"
 }
-
-#variable "vault_user" {
-#  description = "Vault userid: determines location of secrets and affects path of k8s auth backend"
-#}
-
-#variable "vault_addr" {
-#  description = "Address of Vault server including port"
-#}
